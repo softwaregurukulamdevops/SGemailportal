e65c446016905d943976609370b3a319a48a9769
Pramod Kotagiri <PramodKotagiri@softwaregurukulam.com>
2024-08-19 08:10:37 +0000

    Updated container_app_service.tf

--- Terraform_Scripts/container_app_service/container_app_service.tf
+++ Terraform_Scripts/container_app_service/container_app_service.tf
@@ -4,13 +4,13 @@ provider "azurerm" {
 
 # Define the Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "rg-emailportal-container-resources"
+  name     = "rg-emailportal-container-rg"
   location = "eastus"
 }
 
 # Define the Linux App Service Plan for Containers
 resource "azurerm_service_plan" "linux_plan" {
-  name                = "rg-emailportal-linux-app-service-plan"
+  name                = "rg-emailportal-linux-app-svc-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   os_type             = "Linux"
